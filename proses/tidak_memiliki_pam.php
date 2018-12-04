1510d972d3b93f361b6e1308c4ddc37bb60830fa
Jesi Namora <ecimoramora@gmail.com>
2018-12-04 10:10:21 +0700

    Adding DB

--- proses/tidak_memiliki_pam.php
+++ proses/tidak_memiliki_pam.php
@@ -1,6 +1,11 @@
 <?php
 include("connect.php");
-$sql = "select ST_asgeojson(geom) AS geometry,ST_X(ST_centroid(geom)) as x,ST_Y(ST_Centroid(geom)) as y, gid_bangunan,air_pam,id_pemilik_b FROM bangunan WHERE air_pam='Tidak'";
+$sql = "select ST_asgeojson(geom) AS geometry,ST_X(ST_centroid(geom)) as x,ST_Y(ST_Centroid(geom)) as y,penduduk.nama as nama_penduduk, gid_bangunan,bangunan.*,pemilik_bangunan.*,penduduk.*,datuk.*,suku.* FROM bangunan
+INNER JOIN pemilik_bangunan ON bangunan.id_pemilik_b=pemilik_bangunan.id_pemilik_b	
+INNER JOIN penduduk ON pemilik_bangunan.id_penduduk=penduduk.id_penduduk
+INNER JOIN datuk ON penduduk.id_datuk=datuk.id_datuk
+INNER JOIN suku ON suku.id_suku=datuk.id_suku
+WHERE air_pam='Tidak'";
 $result = pg_query($sql);
 $hasil = array(
 	'type'	=> 'FeatureCollection',
@@ -16,7 +21,27 @@ while ($isinya = pg_fetch_assoc($result)) {
 			'air_pam' => $isinya['air_pam'],
             'id_pemilik_b' => $isinya['id_pemilik_b'],
             'x' => $isinya['x'],
-            'y' => $isinya['y']
+			'y' => $isinya['y'],
+			'gid_bangunan' => $isinya['gid_bangunan'],
+			'air_pam' => $isinya['air_pam'],
+            'id_pemilik_b' => $isinya['id_pemilik_b'],
+            'x' => $isinya['x'],
+			'y' => $isinya['y'],
+			
+			'gid_bangunan' => $isinya['gid_bangunan'],
+			'air_pam' => $isinya['air_pam'],
+            'id_pemilik_b' => $isinya['id_pemilik_b'],
+			'nama_penduduk'=>$isinya['nama_penduduk'],
+			'nama_suku'=>$isinya['nama_suku'],
+			'no_kk'=>$isinya['no_kk'],
+			'tgl_lahir'=>$isinya['tgl_lahir'],
+			'pendidikan'=>$isinya['pendidikan'],
+			'penghasilan'=>$isinya['penghasilan'],
+			'asuransi'=>$isinya['asuransi'],
+			'tabungan'=>$isinya['tabungan'],
+			'kapasitas_listrik'=>$isinya['kapasitas_listrik'],
+			'pbb'=>$isinya['pbb']
+			
 			)
 		);
 	array_push($hasil['features'], $features);
